93cf5e089661106b8aed753158b62c9e599c58e3
orischer <80687338+orischer@users.noreply.github.com>
2021-03-24 12:31:31 +0200

    Update print_pattern.c

--- print_pattern.c
+++ print_pattern.c
@@ -1,10 +1,23 @@
 // 123456789
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 #include <stdlib.h>
 
 #define LENGTH 2*n - 1
+
+int pro (int n, int i) {
+    for (int j=0; j<LENGTH; j++) {
+       if (j<i)  {
+                printf("%d", n-j);
+            }
+        else if(j>=i && j<LENGTH-i) {
+                printf ("%d", n-i);
+            }
+        else {
+                printf ("%d", n+j-LENGTH+3);
+            }
+        } 
+    return 0;
+}
 
 int main() 
 {
@@ -13,20 +26,14 @@ int main()
     scanf("%d", &n);
     if (n > 100 || n < 1) return 0;
   
-    for(int i=0; i< LENGTH; i++) {
-        for(int j=0; j< LENGTH; j++)  {
-            if (j<i)  {
-                printf("%d", n-j);
-            }
-            else if(j>=i && j<LENGTH-i) {
-                printf ("%d", n-i);
-            }
-            else {
-                printf ("%d", n+j-LENGTH+1);
-            }
+    for(int i=0; i<LENGTH; i++) {
+        if (i<n){
+            pro (n, i);
+        }
+        else {
+            pro (n, LENGTH-i-1);
         }
         printf("\n");
     }
     return 0;
 }
-
