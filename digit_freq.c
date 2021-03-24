33a56fc4ab8185ab03a85821d5059f46027b2816
rahelpre <81296022+rahelpre@users.noreply.github.com>
2021-03-24 12:49:30 +0200

    Add files via upload

---
+++ digit_freq.c
@@ -0,0 +1,11 @@
+// 123456789
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
