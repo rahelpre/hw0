7c2375df967a8d5054fb601b23f91202d8025b19
rahelpre <81296022+rahelpre@users.noreply.github.com>
2021-03-24 11:24:21 +0200

    Add files via upload

---
+++ print_pattern.c
@@ -0,0 +1,14 @@
+// 123456789
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    int n;
+    scanf("%d", &n);
+    // Complete the code to print the pattern.
+    return 0;
+}
