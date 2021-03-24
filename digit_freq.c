9980b319982da7554cfbc22155a64c129a9d3784
rahelpre <81296022+rahelpre@users.noreply.github.com>
2021-03-24 12:56:56 +0200

    Update digit_freq.c

--- digit_freq.c
+++ digit_freq.c
@@ -5,7 +5,11 @@
 #include <stdlib.h>
 
 int main() {
-
+    int hist[9];
+    char c;
+    c = getc ();
+    while ( c!= NULL && )
+        
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
 }
