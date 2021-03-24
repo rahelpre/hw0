5e60967f086c0d3266e7083b48c2e7bd908df7e7
orischer <80687338+orischer@users.noreply.github.com>
2021-03-24 12:12:36 +0200

    Update print_pattern.c

--- print_pattern.c
+++ print_pattern.c
@@ -4,7 +4,7 @@
 #include <math.h>
 #include <stdlib.h>
 
-#define LENGTH 2*n + 1
+#define LENGTH 2*n - 1
 
 int main() 
 {
@@ -13,9 +13,20 @@ int main()
     scanf("%d", &n);
     if (n > 100 || n < 1) return 0;
   
-    for(int i=0; i< LENGTH; i++)
-        for(int j=0; j< LENGTH; j++)
-            
-        
+    for(int i=0; i< LENGTH; i++) {
+        for(int j=0; j< LENGTH; j++)  {
+            if (j<i)  {
+                printf("%d", n-j);
+            }
+            else if(j>=i && j<LENGTH-i) {
+                printf ("%d", n-i);
+            }
+            else {
+                printf ("%d", n+j-LENGTH+1);
+            }
+        }
+        printf("\n");
+    }
     return 0;
 }
+
