e64e7a83e6636c049bcb207c98ca9daa352deb56
orischer <80687338+orischer@users.noreply.github.com>
2021-03-24 13:20:15 +0200

    Update digit_freq.c

--- digit_freq.c
+++ digit_freq.c
@@ -5,11 +5,17 @@
 #include <stdlib.h>
 
 int main() {
-    int hist[9];
+    int hist[10]={0};
     char c;
-    c = getc ();
-    while ( c!= NULL && )
-        
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    c = getc(stdin);
+    while ( c!= '?'){ \\change this later
+        if (c>='0' && c<'9') {
+            hist[c-'0']++;
+        }
+        c = getc(stdin);
+    }
+    for(int i=0; i<10; i++) {
+        printf("%d", hist[i]); \\change this later
+    }
     return 0;
 }
