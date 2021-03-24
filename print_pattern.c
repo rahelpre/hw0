0082ef8589328c77a38bcf4b56c2814543e18a3e
rahelpre <81296022+rahelpre@users.noreply.github.com>
2021-03-24 11:46:45 +0200

    Update print_pattern.c

--- print_pattern.c
+++ print_pattern.c
@@ -4,13 +4,18 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define LENGTH 2*n + 1
+
 int main() 
 {
 
     int n;
     scanf("%d", &n);
-    // Complete the code to print the pattern.
-    for(int i=0; i<n; i++)
+    if (n > 100 || n < 1) return 0;
+  
+    for(int i=0; i< LENGTH; i++)
+        for(int j=0; j< LENGTH; j++)
+            
         
     return 0;
 }
