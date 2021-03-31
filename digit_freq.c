7d08bc1c2233ca40035c5f55bd42d1a87161c5b3
rahelpre <81296022+rahelpre@users.noreply.github.com>
2021-03-31 21:19:22 +0300

    Update digit_freq.c

--- digit_freq.c
+++ digit_freq.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define NUM_OF_DIG 10
-#define DIG_ST '0'
-#define DIG_END '9'
 
 /**
  * @brief function outpus frequency of each digit in all chars in input.
- * @param c holds current char from input.
- * @note getc gets a new char till EOF 
+ * @variable c holds current char from input.
  */
 
 int main() {
@@ -15,13 +12,14 @@ int main() {
     char c;
     c = getc(stdin);
     while ( c!= EOF){
-        if (c>= DIG_ST && c< DIG_END) {
-            hist[c - DIG_ST]++;
+        if (c>= '0' && c< '9') {
+            hist[c - '0']++;
         }
         c = getc(stdin);
     }
-    for(int i=0; i<NUM_OF_DIG; i++) {
-        putc(hist[i]+DIG_ST, stdout);
+    for(int i=0; i<NUM_OF_DIG-1; i++) {
+        printf("%d ", hist[i]);
     }
+    printf("%d", hist[NUM_OF_DIG-1]);
     return 0;
 }
